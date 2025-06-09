477e7fbf7361c90e5bbdc116fbd9ee79d02c70e2
John Olson <john.olson@waystar.com>
2025-06-09 12:16:52 -0600

    Console connect 4 fully playable, but doesn't check for errors or victory yet

---
+++ Connect4/GridStatus.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect4
+{
+    public class GridStatus
+    {
+        public List<List<int>> Grid { get; set; } =
+        [
+            [0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0]
+        ];
+
+        public bool RedoTurn { get; set; } = false;
+
+        public int ChangePlayer(int player)
+        {
+            if (player == 2) { return 1; } else { return 2; }
+        }
+
+        public int GetLowestRow(int column)
+        {
+            for (int i = 0; i < Grid.Count; i++)
+            {
+                if (Grid[i][column] == 0)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public string AddPiece(int column, int player)
+        {
+            int row = GetLowestRow(column - 1);
+            if (row == -1)
+            {
+                return $"Column {column - 1} is full. Try again\n";
+            }
+            Grid[row][column - 1] = player;
+            return "";
+        }
+
+        public void ShowGrid()
+        {
+            for (int i = 1; i <= Grid.Count; i++)
+            {
+                for (int j = 0; j < Grid[^i].Count; j++)
+                {
+                    Console.Write($"{Grid[^i][j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("\n1 2 3 4 5 6 7");
+        }
+    }
+}
---
+++ Connect4/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Connect4;
+
+Console.WriteLine("Let's Play Connect 4!");
+
+bool gameOver = false;
+int player = 1;
+GridStatus game = new GridStatus();
+
+while (!gameOver)
+{
+    game.ShowGrid();
+    Console.Write($"Player {player}'s turn\nSelect your column: ");
+
+    int columnChoice = Convert.ToInt32(Console.ReadLine());
+    
+    string addResponse = game.AddPiece(columnChoice, player);
+    
+    if (addResponse != "")
+    {
+        game.RedoTurn = true;
+        Console.WriteLine(addResponse);
+
+    }
+
+    if (!game.RedoTurn) { player = game.ChangePlayer(player); }
+    else { game.RedoTurn = false; }
+
+}
+
